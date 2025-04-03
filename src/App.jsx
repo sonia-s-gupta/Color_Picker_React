f10b0a958f9b4be6e35231eba2c76e454b2b1784
Sonia Gupta <soniagupta@Sonias-Air.attlocal.net>
2025-04-03 13:46:57 -0400

    Updated code and deployed to Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-// Write your Color component here
+// Imports useState hook from React
+import { useState } from 'react';
 
-const App = () => {
+//Creates color component, adds onClick property w/ anonymous function passed in,
+//uses object deconstruction
+const Color = ({color, setSelectedColor}) => { 
+  return (
+  <div 
+  className={color}
+  onClick={() => setSelectedColor(color)}
+  ></div>
+  );
+};
+
+//Creates picker parent component and children color components
+const App = () => { 
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color = "red" setSelectedColor={setSelectedColor}/>
+        <Color color = "yellow" setSelectedColor={setSelectedColor}/>
+        <Color color = "blue" setSelectedColor={setSelectedColor}/>
+      </div>
     </div>
   );
 };
